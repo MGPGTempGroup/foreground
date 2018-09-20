7a7cf5d4904df966f1c506d3d3231fcca4dc36bf
arfurs <arfurs@163.com>
2018-09-20 15:11:29 +0800

    init

---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<el-button type="primary">主要按钮</el-button>
+	</div>
+</template>
+<script>
+	import { Button, Select } from 'element-ui';
+	export default{
+		components:[
+			Button,Select
+		]
+		
+	}
+</script>
