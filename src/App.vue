be2f3737a53d4663ac3e36ddb45cbd296bb6d5f7
arfurs <arfurs@163.com>
2018-09-21 19:45:35 +0800

    导航条组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
+	<div class="container" >
+		<navigation />
 		<router-view></router-view>
+	</div>
 </template>
 
 <script>
+	import Navigation from '@/components/Nav'
 	export default {
-		name: "App"
+		name: 'App',
+		components: {
+			Navigation
+		}
 	};
 </script>
+
+<style lang="scss" >
+	body {
+		padding: 0;
+		margin: 0;
+		background-color: #F7F7F7;
+		div {
+			color: red;
+		}
+	}
+</style>
+
