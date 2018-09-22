b882ca72acf553301c5ae44cb81983c4aa5a4e4e
awagege <840442707@qq.com>
2018-09-23 05:04:21 +0800

    新增组件MainBox
    新增组件MainBox

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,46 +1,55 @@
 <template>
-  <main class="container" >
-    <div class="carousel" >
-      <el-carousel class="carousel" :interval="5000" height="500px">
-        <el-carousel-item class="carousel-item" v-for="item in 4" :key="item">
-          <img src="@/assets/images/banner/Home_Hero_1.1.gif" alt="">
-          <h2>&nbsp;One step ahead.</h2>
-        </el-carousel-item>
-      </el-carousel>
-    </div>
-  </main>
+	<main class="container">
+		<div class="carousel">
+			<el-carousel class="carousel" :interval="5000" height="500px">
+				<el-carousel-item class="carousel-item" v-for="item in 4" :key="item">
+					<img src="@/assets/images/banner/Home_Hero_1.1.gif" alt="">
+					<h2>&nbsp;One step ahead.</h2>
+				</el-carousel-item>
+			</el-carousel>
+		</div>
+		<el-row :gutter="20">
+			<main-box v-for="(item,index) in 9" :key="index"></main-box>
+		</el-row>
+	</main>
 </template>
 
 <script>
-export default {
-	name: 'home-page'
-}
+ import MainBox from '@/components/MainBox'
+	export default {
+		components:{
+			MainBox
+		},
+		name: 'home-page'
+	}
 </script>
 
-<style scoped lang="scss" >
-  .container {
-    .carousel {
-      margin: 0 auto;
-      margin-top: 50px;
-      &-item {
-        img {
-          width: 100%;
-          height: 100%;
-        }
-        h2 {
-          position: absolute;
-          top: 140px;
-          z-index: 100;
-          width: 100%;
-          text-align: center;
-          font-size: 50px;
-          letter-spacing: 2px;
-          text-shadow: 2px 2px 10px black;
-          color: white;
-        }
-      }
-    }
-  }
+<style scoped lang="scss">
+	.container {
+		.carousel {
+			margin: 0 auto;
+			margin-top: 50px;
+
+			&-item {
+				img {
+					width: 100%;
+					height: 100%;
+				}
+
+				h2 {
+					position: absolute;
+					top: 140px;
+					z-index: 100;
+					width: 100%;
+					text-align: center;
+					font-size: 50px;
+					letter-spacing: 2px;
+					text-shadow: 2px 2px 10px black;
+					color: white;
+				}
+			}
+		}
+	}
 </style>
 
 
