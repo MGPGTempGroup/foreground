27c17d1ba9cd953603d50b4840872bfb75e8b01a
arfurs <arfurs@163.com>
2018-09-23 21:05:29 +0800

    landlords响应式设置

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 
 import home from '@/views/home'
 import awagegedemo from '@/views/awagegedemo'
+import landlords from '@/views/property/landlords'
 Vue.use(Router)
 export default new Router({
 	mode: 'history',
@@ -10,7 +11,13 @@ export default new Router({
 		path: '/',
 		name: 'home',
 		component: home
-	}, {
+	},
+	{
+		path: '/landlords',
+		name: 'home',
+		component: landlords
+	},
+	{
 		path: '/awagegedemo',
 		name: 'awagegedemo',
 		component: awagegedemo
