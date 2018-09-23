76d0182f18200814788da01ff51a72166adffe57
arfurs <arfurs@163.com>
2018-09-24 00:13:05 +0800

    areas-we-serve页面

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,12 @@ import Router from 'vue-router'
 
 import home from '@/views/home'
 import awagegedemo from '@/views/awagegedemo'
+
+// 物业管理服务相关
 import landlords from '@/views/property/landlords'
 import prospectiveTenants from '@/views/property/prospectiveTenants'
 import currentTenants from '@/views/property/currentTenants'
+import areasWeServe from '@/views/property/areasWeServe'
 
 Vue.use(Router)
 export default new Router({
@@ -29,6 +32,11 @@ export default new Router({
 		path: '/current-tenants',
 		name: 'currentTenants',
 		component: currentTenants
+	},
+	{
+		path: '/areas-we-serve',
+		name: 'areasWeServe',
+		component: areasWeServe
 	},
 	{
 		path: '/awagegedemo',
---
+++ src/views/property/areasWeServe.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="areas-we-serve">
+    <div class="carousel" >
+      <el-carousel class="carousel" :interval="5000" height="500px">
+        <el-carousel-item class="carousel-item" v-for="item in 4" :key="item">
+          <img src="@/assets/images/banner/Home_Hero_1.1.gif" alt="">
+          <h2>&nbsp;One step ahead.</h2>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
+    <el-row class="content" >
+      <el-col class="content-desc">
+        <p>
+          At Melbourne Real Estate we understand that when you are looking to buy, rent or invest in real estate it’s a big decision. It is important to know and understand not only the market place but also who lives in the area, what schools, public transport and activities the areas has to offer.
+        </p>
+        <p>
+          Below you will find a selection of some of Melbourne’s best suburbs, to see a full list of areas we serve Click Here. If you have any questions, feel free to contact one of our agents!
+        </p>
+      </el-col>
+    </el-row>
+    <!-- 服务地区列表 -->
+    <el-row tag="ul" class="areas-list" >
+      <el-col
+        class="areas-list-item"
+        v-for="(item, index) in 6"
+        :key="index"
+        tag="li"
+        :xs="24" :sm="12" :md="8" :lg="8" >
+        <main-box />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import MainBox from '@/components/MainBox'
+  export default {
+    name: 'areas-we-serve-page',
+    components: {
+      MainBox
+    }
+  }
+</script>
+
+<style scoped lang="scss" >
+  @import '@/assets/sass/mixins/responsive.scss';
+  .areas-we-serve {
+    padding: 35px 0px 70px 0px;
+    .carousel {
+      margin: 0 auto;
+      &-item {
+        img {
+          width: 100%;
+          height: 100%;
+        }
+        h2 {
+          position: absolute;
+          top: 140px;
+          z-index: 100;
+          width: 100%;
+          text-align: center;
+          font-size: 50px;
+          letter-spacing: 2px;
+          text-shadow: 2px 2px 10px black;
+          color: white;
+        }
+      }
+    }
+    .content {
+      margin-top: 20px;
+      &-desc {
+        > p {
+          font-size: 18px;
+          color: #343434;
+        }
+        @include media-xs {
+          padding: 0px 25px !important;
+        }
+        @include media-sm {
+          padding: 0px 30px !important;
+        }
+        @include media-md {
+          padding: 0px 40px !important;
+        }
+        @include media-custom(1200px, 1330px) {
+          padding: 0px 30px !important;
+        }
+        @include media-sm-down {
+          img {
+            width: 100%;
+          }
+        }
+      }
+    }
+    .areas-list {
+      &-item {
+        @include media-xs {
+          padding: 0px 20px;
+        }
+        @include media-sm {
+          padding: 0px 20px;
+          &:nth-of-type(2n) {
+            padding-left: 10px;  
+          }
+          &:nth-of-type(2n + 1) {
+            padding-right: 10px;
+          }
+        }
+        @include media-md {
+          padding: 0px 20px;
+        }
+        @include media-lg {
+          padding: 0px 10px;
+          &:nth-of-type(3n) {
+            padding-right: 0px;  
+          }
+          &:nth-of-type(2n) {
+            padding: 0px 10px;
+          }
+          &:nth-of-type(3n + 1) {
+            padding-left: 0px;
+          }
+        }
+        @include media-custom(1200px, 1300px) {
+          padding: 0px 20px !important;
+        }
+      }
+    }
+  }
+</style>
