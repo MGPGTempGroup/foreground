7675b9d38cd2aa1c264cdf5dab52ae5eeba6d4ab
awagege <840442707@qq.com>
2018-09-24 08:39:10 +0800

    返回顶部按钮
    返回顶部按钮

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-	<div class="container" >
-		
+	<div class="container" @touchmove="scroll">
+
 		<!-- 头部 -->
 		<header-top />
 
@@ -10,15 +10,18 @@
 		<!-- 轮播公告信息 -->
 		<notice-marquee :list="noticeList" />
 
-		<router-view class="router-view"  />
+		<router-view class="router-view" />
 
 		<!-- 底部介绍与版权相关信息 -->
 		<bottom-footer />
+		<!-- 返回顶部 -->
+		<div class="to-top" ref="toTop" @click="toTop">
+			<i class="el-icon-arrow-up"></i>
+		</div>
 	</div>
 </template>
 
 <script>
-
 	// 导入组件
 	import Navigation from '@/components/Nav'
 	import HeaderTop from '@/components/HeaderTop'
@@ -35,29 +38,97 @@
 		},
 		data() {
 			return {
-				noticeList: [
-					{ content: 'xxxxx', rate: 1.1 },
-					{ content: 'xxxxx', rate: 2.1 },
-					{ content: 'xxxxx', rate: 4.1 },
-					{ content: 'xxxxx', rate: 5 },
-					{ content: 'xxxxx', rate: 1 },
-					{ content: 'xxxxx', rate: 3 },
-					{ content: 'xxxxx', rate: 4 },
-					{ content: 'xxxxx', rate: 2 },
-					{ content: 'xxxxx', rate: 5 },
-					{ content: 'xxxxx', rate: 1 },
-					{ content: 'xxxxx', rate: 1 },
-					{ content: 'xxxxx', rate: 5 },
-					{ content: 'xxxxx', rate: 5 },
-					{ content: 'xxxxx', rate: 5 },
-					{ content: 'xxxxx', rate: 5 }
+				noticeList: [{
+						content: 'xxxxx',
+						rate: 1.1
+					},
+					{
+						content: 'xxxxx',
+						rate: 2.1
+					},
+					{
+						content: 'xxxxx',
+						rate: 4.1
+					},
+					{
+						content: 'xxxxx',
+						rate: 5
+					},
+					{
+						content: 'xxxxx',
+						rate: 1
+					},
+					{
+						content: 'xxxxx',
+						rate: 3
+					},
+					{
+						content: 'xxxxx',
+						rate: 4
+					},
+					{
+						content: 'xxxxx',
+						rate: 2
+					},
+					{
+						content: 'xxxxx',
+						rate: 5
+					},
+					{
+						content: 'xxxxx',
+						rate: 1
+					},
+					{
+						content: 'xxxxx',
+						rate: 1
+					},
+					{
+						content: 'xxxxx',
+						rate: 5
+					},
+					{
+						content: 'xxxxx',
+						rate: 5
+					},
+					{
+						content: 'xxxxx',
+						rate: 5
+					},
+					{
+						content: 'xxxxx',
+						rate: 5
+					}
 				]
 			}
+		},
+		methods: {
+			toTop() {
+				Velocity(document.body, "scroll")
+			},
+			scroll() {
+				//console.log(this.$refs.toTop.style.bottom)
+				if(this.$refs.toTop.style.bottom!='100px' && this.$refs.toTop.style.bottom!='-100px' && this.$refs.toTop.style.bottom!=''){
+					return false
+				}
+				Velocity(this.$refs.toTop, "stop")
+				if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+					Velocity(this.$refs.toTop, { bottom: 100 })
+				} else {
+					Velocity(this.$refs.toTop, { bottom: -100 })
+				}
+			}
+		},
+		created() {
+			this.$nextTick(() => {
+				window.onscroll = () => {
+					this.scroll()
+				}
+			})
 		}
 	};
 </script>
 
-<style scoped lang="scss" >
+<style scoped lang="scss">
 	.container {
 		.router-view {
 			padding-top: 30px;
@@ -65,6 +136,20 @@
 			max-width: 1263px;
 			min-height: calc(100vh - 155.33px - 60px - 35px - 458px);
 		}
+
+		.to-top {
+			position: fixed;
+			bottom: -100px;
+			right: 20px;
+			width: 50px;
+			height: 50px;
+			border-radius: 50px;
+			background: #000;
+			text-align: center;
+			line-height: 50px;
+			box-shadow: 0px 0px 10px #ccc;
+			z-index: 1000;
+			color: #fff;
+		}
 	}
 </style>
-
