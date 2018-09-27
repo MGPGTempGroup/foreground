f6dc090fab963685b9ba42c3d6431a2795263e89
awagege <840442707@qq.com>
2018-09-28 02:56:48 +0800

    Merge branch 'dev' of https://github.com/MGPGTempGroup/front-end-fd into dev

--- src/views/commercial/index.vue
+++ src/views/commercial/index.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
 	<main class="commercial">
 		<main-swiper></main-swiper>
 		<el-row class="content">
@@ -32,24 +31,7 @@
 					that murky publicity and confusing information serves no one, so we cut to the chase, delivering a tailored
 					campaign clearly and honestly.
 					Along with our on-line booking application for inspection times that suit the tenant, our virtual video tours and
-					professional photography accurately showcase each property, displaying relevant features and benefits.
-=======
-  <main class="commercial" >
-    <main-swiper></main-swiper>
-    <el-row>
-      <el-col class="commercial-content" :lg="18" >
-        <el-card>
-          <p><strong>Melbourne Real Estate is leading the way in Commercial Property Management</strong></p>
-          <p>We bring our knowledge, expertise, contacts and award-winning business practices to deliver an unprecedented level of service to every client every day.</p>
-          <p>Asset Managers <a href="https://www.melbournerealestate.com.au/agent/brenton-reginato/">Brenton Reginato</a> and <a href="https://www.melbournerealestate.com.au/agent/samantha-bolton/">Samantha Bolton</a>, two highly experienced professionals in both the residential and commercial sectors, along with Commercial Leasing Consultant <a href="https://www.melbournerealestate.com.au/agent/mike-drover/">Mike Drover</a> bring integrity, accountability and a genuine passion for property to their respective roles, ensuring strong and lasting industry connections.</p>
-          <h2>&nbsp;<img class="alignleft wp-image-8111" src="https://www.melbournerealestate.com.au/wp-content/uploads/2018/04/Commercial-Property-Management-Images-01-640x380.jpg" alt="Melbourne Real Estate’s reputation for shaking up traditional modes of operating to produce exemplary results extends into every facet of the business, including our Commercial division." width="512" height="304" srcset="https://www.melbournerealestate.com.au/wp-content/uploads/2018/04/Commercial-Property-Management-Images-01-640x380.jpg 640w, https://www.melbournerealestate.com.au/wp-content/uploads/2018/04/Commercial-Property-Management-Images-01-768x457.jpg 768w, https://www.melbournerealestate.com.au/wp-content/uploads/2018/04/Commercial-Property-Management-Images-01-1024x609.jpg 1024w" sizes="(max-width: 512px) 100vw, 512px"></h2>
-          <p>Raising the bar begins with our highly transparent advertising practices. We understand that time is money and that murky publicity and confusing information serves no one, so we cut to the chase, delivering a tailored campaign clearly and honestly.</p>
-          <p>Along with our on-line booking application for inspection times that suit the tenant, our virtual video tours and professional photography accurately showcase each property, displaying relevant features and benefits.</p>
-          <p>Melbourne Real Estate’s Asset Managers supervise each and every tenant transaction, setting up direct debit payment options for rent to avoid rental arrears and ensuring that 100% of all tenant property outgoings are paid through our office.</p>
-          <p>We are committed to continually evolving technologies, dedicated customer service, extensive market reach and the seamless execution of managing your property.</p>
-          Raising the bar begins with our highly transparent advertising practices. We understand that time is money and that murky publicity and confusing information serves no one, so we cut to the chase, delivering a tailored campaign clearly and honestly.
-          Along with our on-line booking application for inspection times that suit the tenant, our virtual video tours and professional photography accurately showcase each property, displaying relevant features and benefits.
->>>>>>> b4cbc56725d12165202d13f5b6352323a1d7e804
+					professional photography accurately showcase each property, displaying relevant features and benefits
 
 					Melbourne Real Estate’s Asset Managers supervise each and every tenant transaction, setting up direct debit
 					payment options for rent to avoid rental arrears and ensuring that 100% of all tenant property outgoings are paid
@@ -72,7 +54,6 @@
 </template>
 
 <script>
-<<<<<<< HEAD
 	import MainSwiper from '@/components/MainSwiper'
 	import CommonForm from '@/components/CommonForm'
 	export default {
@@ -105,65 +86,23 @@
 				margin-top: 10px;
 				padding: 0 10px;
 			}
-		}
-=======
-  import CommonForm from '@/components/CommonForm'
-  import MainSwiper from '@/components/MainSwiper'
-  export default {
-    name: 'commercial-page',
-    components: {
-      CommonForm,
-      MainSwiper
-    }
-  }
-</script>
 
-<style scoped lang="scss" >
-  @import '@/assets/sass/mixins/responsive.scss';
-  .commercial {
-    padding-bottom: 33px;
-    &-content, &-form {
-      margin-top: 30px;
-      @include media-md-down {
-        padding: 0px 10px !important;
-      }
-    }
-    &-content {
-      padding-right: 32px;
-      > * {
-        padding: 10px 10px;
-      }
-      @include media-sm-down {
-        img {
-          width: 100%;
-        }
-      }
-    }
-  }
-</style>
->>>>>>> b4cbc56725d12165202d13f5b6352323a1d7e804
+			padding-bottom: 33px;
 
-		&-form {
-			@include media-md {
-				margin-top: 30px;
-			}
 
-			@include media-sm {
-				margin-top: 20px;
-			}
 
-			@include media-xs {
-				margin-top: 10px;
-			}
-		}
-
-		&-content {
-			>* {
-				padding: 10px 10px;
-			}
-
-			@include media-lg-up {
+			&-content {
 				padding-right: 32px;
+
+				>* {
+					padding: 10px 10px;
+				}
+
+				@include media-sm-down {
+					img {
+						width: 100%;
+					}
+				}
 			}
 		}
 	}
