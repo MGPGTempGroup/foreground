7573c1aa8787c8c22f07d0276cd887af854312ee
arfurs <arfurs@163.com>
2018-09-27 13:12:42 +0800

    服务评价组件

---
+++ src/components/ServiceComment/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <component class="comment-container" :is="tag" >
+    <el-rate
+      v-model="rate"
+      disabled
+      show-score
+      text-color="#ff9900"
+      score-template="{value}">
+    </el-rate>
+    <div class="info" >
+      <b>{{ name }}</b> - {{ date }}
+      <br />
+      <a href="//www.melgrand.com">Via</a>
+    </div>
+    <div class="content" >
+      <slot></slot>
+    </div>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'service-comment',
+  props: {
+    tag: {
+      type: String,
+      default: 'div'
+    },
+    rate: {
+      type: Number,
+      default: 5 // 默认评分5
+    },
+    name: String,
+    date: String
+  }
+}
+</script>
+
+<style scoped lang="scss" >
+  .comment-container {
+    width: 100%;
+  }
+  .info {
+    margin-top: 10px;
+  }
+  .content {
+    padding: 10px 0px 10px 0px;
+    letter-spacing: 0.5px;
+    color: #272822;
+  }
+</style>
+
+
