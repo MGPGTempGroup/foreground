58aa8f91ae40beb322187f9375ae3d37ab4674f4
awagege <840442707@qq.com>
2018-09-28 04:00:04 +0800

    最后一个边距
    最后一个边距

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,11 @@
 <template>
-	<main class="container">
+	<main class="home">
 		<main-swiper></main-swiper>
-		<el-row  class="content">
-			<main-box v-for="(item,index) in 9" :key="index"></main-box>
-		</el-row>
+		<div class="content">
+			<el-row gutter='20'>
+				<main-box v-for="(item,index) in 9" :key="index"></main-box>
+			</el-row>
+		</div>
 	</main>
 </template>
 
@@ -20,7 +22,26 @@
 </script>
 
 <style scoped lang="scss">
-	.container {}
+	
+		@import '@/assets/sass/mixins/responsive.scss';
+	.home {
+		.content {
+			margin-top: 35px;
+
+			@include media-md {
+				margin-top: 30px;
+			}
+
+			@include media-sm {
+				margin-top: 20px;
+			}
+
+			@include media-xs {
+				margin-top: 10px;
+				padding: 0 10px;
+			}
+		}
+	}
 </style>
 
 
